5404309b36c1c622e9dfab92f160a1a8c99e1551
CamilleDupre <47179915+CamilleDupre@users.noreply.github.com>
2021-02-15 11:20:46 +0100

    object spwaner

---
+++ Assets/ObjectSpawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public List<GameObject> objects;
+
+    public void Spawn(string objectName)
+    {
+        foreach (var item in objects)
+        {
+            item.SetActive(objectName == item.name);
+        }
+    }
+}
+
+
