0037fa0291ad12484de7317fc4aa0f96d0db1ddc
Johann Equilbec <johann.equilbec@universite-paris-saclay.fr>
2021-02-19 16:31:08 +0100

    Menu in creation

---
+++ Assets/LineRendererSettings.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LineRendererSettings : MonoBehaviour
+{
+    [SerializeField] LineRederer rend;
+    Vector3[] points;
+
+    public LayerMask layerMask;
+    public GameObject panel;
+    public Image img;
+    public Button btn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = gameObject.GetComponent<LineRenderer>();
+
+        points = new Vector3[2];
+
+        points[0] = Vector3.zero;
+
+        points[1] = transform.positioon + new Vector3(0, 0, 20);
+
+        rend.SetPositions(points);
+
+        rend.enabled = true;
+    }
+    
+    public void AlignLineRenderer(LineRendererSettings rend)
+    {
+        Ray ray;
+        RaycastHit hit;
+
+        ray = new Ray(transform.position, transform.forward);
+
+        if (Physics.Raycast(ray, out hit, layerMask)) {
+            points[1] = transform.forward + new Vector3(0, 0, hit.distance);
+            rend.startColor = Color.red;
+            rend.endColor = Color.red;
+        }
+        else
+        {
+            points[1] = transform.forward + new Vector3(0, 0, 20);
+            rend.startColor = Color.green;
+            rend.endColor = Color.green;
+        }
+
+        rend.SetPositions(points);
+
+        rend.material.color = rend.startColor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AlignLineRenderer(rend);
+    }
+}
---
+++ Assets/Scripts/drawing1.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class drawing : MonoBehaviour
+{
+    public AudioClip painting;
+
+    AudioSource m_MyAudioSource;
+
+
+    public OVRInput.Button drawInput;
+    public Transform drawPositionSource;
+    public float lineWidth = 0.03f;
+    public Material lineMaterial;
+    public float distanceThreshold = 0.05f;
+
+    private List<Vector3> currentLinePositions = new List<Vector3>();
+    private XRController controller;
+    private bool isDrawing = false;
+    private bool isPressed = false;
+    private LineRenderer currentLine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<XRController>();
+        m_MyAudioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Check if input down
+        //OVRInput.Button.IsPressed(controller.inputDevice, drawInput, out bool isPressed);
+        if (OVRInput.Get(OVRInput.Button.One) == true)
+        {
+            isPressed = true;
+        }
+        else
+        {
+            isPressed = false;
+        }
+
+        if (!isDrawing && isPressed)
+        {
+            StartDrawing();
+        }
+        else if (isDrawing && !isPressed)
+        {
+            StopDrawing();
+        }
+        else if (isDrawing && isPressed)
+        {
+            UpdateDrawing();
+        }
+    }
+
+    public void SetLineMaterial(Material newMat)
+    {
+        lineMaterial = newMat;
+    }
+
+    void StartDrawing()
+    {
+        isDrawing = true;
+        //create line
+        GameObject lineGameObject = new GameObject("Line");
+        currentLine = lineGameObject.AddComponent<LineRenderer>();
+
+        UpdateLine();
+        //MakeSound(painting);
+        m_MyAudioSource.Play();
+    }
+
+    void UpdateLine()
+    {
+        //update line
+        //update line position
+        currentLinePositions.Add(drawPositionSource.position);
+        currentLine.positionCount = currentLinePositions.Count;
+        currentLine.SetPositions(currentLinePositions.ToArray());
+
+        //update line visual
+        currentLine.material = lineMaterial;
+        currentLine.startWidth = lineWidth;
+    }
+
+    void StopDrawing()
+    {
+        isDrawing = false;
+        currentLinePositions.Clear();
+        currentLine = null;
+        m_MyAudioSource.Stop();
+    }
+
+    void UpdateDrawing()
+    {
+        //check if we have a line
+        if (!currentLine || currentLinePositions.Count == 0)
+            return;
+
+        Vector3 lastSetPosition = currentLinePositions[currentLinePositions.Count - 1];
+        if (Vector3.Distance(lastSetPosition, drawPositionSource.position) > distanceThreshold)
+        {
+            UpdateLine();
+        }
+        
+    }
+}
